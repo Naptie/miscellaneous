136a18802dc41612da24b69f193c6a289059a3fa
Naptie <naptie@qq.com>
2025-11-17 15:42:05 +0800

    init

---
+++ src/routes/hearing-test/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import AudioSignalGenerator from '$lib/components/AudioSignalGenerator.svelte';
+</script>
+
+<div class="bg-base-200 flex min-h-screen items-center justify-center p-4">
+  <AudioSignalGenerator />
+</div>
